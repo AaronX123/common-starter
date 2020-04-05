1cccd45927a0897e883bda3cc650ac50866db84a
xiaoyouming <739243573@qq.com>
2020-04-05 17:50:19 +0800

    (+) 将CacheConstants移至基础框架包中

---
+++ common-data/src/main/java/aaron/common/data/common/CacheConstants.java
@@ -0,0 +1,17 @@
+package aaron.common.data.common;
+
+/**
+ * 用于获取公共库属性
+ * @author xiaoyouming
+ * @version 1.0
+ * @since 2020-04-05
+ */
+public class CacheConstants {
+    public static final String CATEGORY = "category";
+    public static final String DICTIONARY = "dictionary";
+    public static final String PAPER = "paper";
+    public static final String PAPER_DETAIL = "paper_detail";
+    public static final String USER = "user";
+    public static final String ORG = "org";
+    public static final String COMPANY = "company";
+}
