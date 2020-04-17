46d4de8aeb0ef9efd2fc32ed77b9a0ccdd4cdb9a
xiaoyouming <739243573@qq.com>
2020-04-17 17:19:45 +0800

    (*) 增加跟踪码

--- common-data/src/main/java/aaron/common/data/common/CommonExceptionHandler.java
+++ common-data/src/main/java/aaron/common/data/common/CommonExceptionHandler.java
@@ -2,6 +2,7 @@ package aaron.common.data.common;
 
 import aaron.common.data.exception.NestedExamException;
 import aaron.common.data.exception.StarterError;
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import com.netflix.hystrix.exception.HystrixBadRequestException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,7 +36,7 @@ public class CommonExceptionHandler {
     public CommonResponse<String> examException(NestedExamException e){
         log.error("业务异常：{}",e.getMessage());
         log.error("异常码：{}",e.getErrorCode());
-        return new CommonResponse<String>(state.getVersion(),e.getErrorCode(),e.getMessage(),state.FAIL);
+        return new CommonResponse<String>(state.getVersion(),generateTraceCode(e.getErrorCode()),e.getMessage(),state.FAIL);
     }
 
     @ExceptionHandler(Throwable.class)
@@ -44,7 +45,7 @@ public class CommonExceptionHandler {
         log.error("业务异常：{}",StarterError.SYSTEM_UNKNOWN_ERROR.getMsg());
         log.error("真实异常：",t);
         log.error("异常码：{}",StarterError.SYSTEM_UNKNOWN_ERROR.getCode());
-        return new CommonResponse<String>(state.getVersion(),StarterError.SYSTEM_UNKNOWN_ERROR.getCode(),StarterError.SYSTEM_UNKNOWN_ERROR.getMsg(),state.FAIL);
+        return new CommonResponse<String>(state.getVersion(),generateTraceCode(StarterError.SYSTEM_UNKNOWN_ERROR.getCode()),StarterError.SYSTEM_UNKNOWN_ERROR.getMsg(),state.FAIL);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -54,7 +55,7 @@ public class CommonExceptionHandler {
         log.error("参数校验异常:",ex);
         FieldError fieldError = ex.getBindingResult().getFieldError();
         CommonResponse commonError = new CommonResponse();
-        commonError.setCode(StarterError.SYSTEM_PARAMETER_VALUE_INVALID.getCode());
+        commonError.setCode(generateTraceCode(StarterError.SYSTEM_UNKNOWN_ERROR.getCode()));
         commonError.setMsg(String.format(StarterError.SYSTEM_PARAMETER_VALUE_INVALID.getMsg(),fieldError.getField(),fieldError.getDefaultMessage()));
         return commonError;
     }
@@ -65,7 +66,7 @@ public class CommonExceptionHandler {
     public CommonResponse handleBindException(MissingServletRequestParameterException ex){
         log.error("必填校验异常:",ex);
         CommonResponse commonError = new CommonResponse();
-        commonError.setCode(StarterError.SYSTEM_PARAMETER_VALUE_INVALID.getCode());
+        commonError.setCode(generateTraceCode(StarterError.SYSTEM_PARAMETER_VALUE_INVALID.getCode()));
         commonError.setMsg(String.format(StarterError.SYSTEM_PARAMETER_VALUE_INVALID.getMsg(),ex.getParameterName(),ex.getMessage()));
         return commonError;
     }
@@ -77,8 +78,14 @@ public class CommonExceptionHandler {
         log.error("微服务调用异常:",ex);
         CommonResponse commonError = new CommonResponse();
         String[] errors = ex.getMessage().split(":");
-        commonError.setCode(errors[0]);
+        commonError.setCode(generateTraceCode(errors[0]));
         commonError.setMsg(ex.getMessage().substring(errors[0].length()+1));
         return commonError;
+    }
+
+    private String generateTraceCode(String errorCode){
+        String res = errorCode + ":" + IdWorker.get32UUID().substring(0,8);
+        log.error("跟踪码为:{}，业务异常码为:{}",res,errorCode);
+        return res;
     }
 }
