95b6c4a3815d47a5af3656117138065e88a6d48d
xiaoyouming <739243573@qq.com>
2020-05-09 13:57:06 +0800

    (*) 最终演示版v1

--- common-utils/src/main/java/aaron/common/utils/RPCUtils.java
+++ common-utils/src/main/java/aaron/common/utils/RPCUtils.java
@@ -4,6 +4,11 @@ import aaron.common.data.common.CommonResponse;
 import aaron.common.data.exception.StarterError;
 import aaron.common.data.exception.StarterException;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * @author xiaoyouming
@@ -23,7 +28,43 @@ public class RPCUtils {
 
     public static <T> T parseResponse(CommonResponse response, Class<T> resultType, int RPC_TYPE){
         if (isSuccess(response)){
+            // 如果是集合类型
+            if (Collection.class.isAssignableFrom(resultType)){
+                // JSONObject类型
+                Collection<T> result = (Collection<T>) JSON.parseObject(JSON.toJSONString(response.getData()),resultType);
+                for (T t : result) {
+                    t = JSON.parseObject(JSON.toJSONString(t),resultType);
+                }
+                return (T) result;
+            }
             return JSON.parseObject(JSON.toJSONString(response.getData()),resultType);
+        }
+        switch (RPC_TYPE) {
+            case BASEINFO:
+                throw new StarterException(StarterError.SYSTEM_API_ERROR_TYPE, "基础信息服务", response.getMsg());
+            case USER:
+                throw new StarterException(StarterError.SYSTEM_API_ERROR_TYPE, "用户服务", response.getMsg());
+            case PAPER:
+                throw new StarterException(StarterError.SYSTEM_API_ERROR_TYPE,"试卷服务",response.getMsg());
+            case EXAM:
+                throw new StarterException(StarterError.SYSTEM_API_ERROR_TYPE,"考试服务",response.getMsg());
+            case AUTH:
+                throw new StarterException(StarterError.SYSTEM_API_ERROR_TYPE,"认证服务",response.getMsg());
+            default:
+                throw new StarterException(StarterError.SYSTEM_API_ERROR,response.getMsg());
+        }
+    }
+
+    public static <T> Collection<T> parseCollectionTypeResponse(CommonResponse response, Class<T> resultType, int RPC_TYPE){
+        if (isSuccess(response)){
+            // JSONObject类型
+            Collection<T> result = JSON.parseObject(JSON.toJSONString(response.getData()),Collection.class);
+            Collection<T> res = new ArrayList<>();
+            for (T t : result) {
+                t = JSON.parseObject(JSON.toJSONString(t),resultType);
+                res.add(t);
+            }
+            return res;
         }
         switch (RPC_TYPE) {
             case BASEINFO:
